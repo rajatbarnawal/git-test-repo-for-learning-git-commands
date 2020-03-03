7116bf67071fb13aec3d02a0fc48b3f13e22b1e1
rajatbarnawal <king-kong@gmail.com>
2020-03-03 11:53:48 +0530

    First Commit

--- src/T.java
+++ src/T.java
@@ -4,9 +4,11 @@ public class T {
 	public static void main(String[] args) {
 
 		int num1 = 5, num2 = 15, sum;
+		int sum2;
 		sum = num1 + num2;
+		sum2=sum+num1;
 
-		System.out.println("Sum of these numbers: " + sum);
+		System.out.println("Sum of these numbers: " + sum + sum2);
 	}
 
 }
